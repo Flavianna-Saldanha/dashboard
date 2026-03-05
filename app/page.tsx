58a659ba4db9a06eff0b03328f60a39dbceaa117
Flavianna Saldanha <flaviannasaldanha@gmail.com>
2026-03-04 23:19:06 -0300

    add `home`

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,65 @@
 import Link from "next/link";
+import {
+  Menubar,
+  MenubarCheckboxItem,
+  MenubarContent,
+  MenubarGroup,
+  MenubarItem,
+  MenubarMenu,
+  MenubarRadioGroup,
+  MenubarRadioItem,
+  MenubarSeparator,
+  MenubarShortcut,
+  MenubarSub,
+  MenubarSubContent,
+  MenubarSubTrigger,
+  MenubarTrigger,
+} from "@/components/ui/menubar";
+
 
 export default function Home() {
   return (
     <div className="flex flex-col gap-4 p-10">
       <h1 className="text-2xl font-bold">Home</h1>
 
-      <Link href="/dashboard" className="text-blue-500 underline">
-        Ir para Dashboard
-      </Link>
+			<Menubar className="w-40">
+      <MenubarMenu>
+        <MenubarTrigger>Login</MenubarTrigger>
+        <MenubarContent>
+          <MenubarGroup>
+            <MenubarItem>
+            	<Link href="/login">
+								Ir para Login
+							</Link>
+            </MenubarItem>
+            <MenubarItem>
+             <Link href="/signup">
+								Ir para Cadastro
+							</Link>
+            </MenubarItem>
+          </MenubarGroup>
+        </MenubarContent>
+      </MenubarMenu>
 
-			<Link href="/login" className="text-blue-500 underline">
-        Ir para Login
-      </Link>
+      <MenubarMenu>
+        <MenubarTrigger>Dashboard</MenubarTrigger>
+        <MenubarContent>
+          <MenubarGroup>
+            <MenubarItem>
+              <Link href="/dashboard">
+								Ir para Dashboard
+							</Link>
+            </MenubarItem>  
+          </MenubarGroup>
+        </MenubarContent>
+      </MenubarMenu>
+    </Menubar>
 
-      <Link href="/signup" className="text-blue-500 underline">
-        Ir para Cadastro
-      </Link>
+      
+
+			
+
+      
     </div>
   );
 }
