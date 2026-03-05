ee584eff3466bb2a9324995634fc4aa8012bdc39
Flavianna Saldanha <flaviannasaldanha@gmail.com>
2026-03-04 23:02:39 -0300

    add `home`

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,21 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-			
-		</div>
+    <div className="flex flex-col gap-4 p-10">
+      <h1 className="text-2xl font-bold">Home</h1>
+
+      <Link href="/dashboard" className="text-blue-500 underline">
+        Ir para Dashboard
+      </Link>
+
+			<Link href="/login" className="text-blue-500 underline">
+        Ir para Login
+      </Link>
+
+      <Link href="/signup" className="text-blue-500 underline">
+        Ir para Cadastro
+      </Link>
+    </div>
   );
 }
